9dc93617bd876c10e5899e487b65022d49ef71c1
Усачев Дмитрий Дмитриевич <92usachevdd@region.cbr.ru>
2021-05-15 21:37:13 +0300

    #7 Сделать структуру spring boot приложения
    Сформирована структура spring boot приложения
    
    Close #5

---
+++ src/main/java/ru/usachev/wishboard/WishboardApplication.java
@@ -0,0 +1,13 @@
+package ru.usachev.wishboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishboardApplication.class, args);
+	}
+
+}
