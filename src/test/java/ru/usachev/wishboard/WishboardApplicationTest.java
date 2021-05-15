07539bed321aec766ad8357cdc9b76f10d58b813
Усачев Дмитрий Дмитриевич <92usachevdd@region.cbr.ru>
2021-05-15 21:37:13 +0300

    #7 Сделать структуру spring boot приложения
    Сформирована структура spring boot приложения
    
    Close #5

---
+++ src/test/java/ru/usachev/wishboard/WishboardApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.usachev.wishboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishboardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
