a16031e446196b594ded5dccea5a01482f7e1568
Усачев Дмитрий Дмитриевич <92usachevdd@region.cbr.ru>
2021-07-30 18:18:56 +0300

    #26 Реализовать классы из пакета entities согласно диаграмме
    Реализованы классы
    
    Closes #26

--- src/main/java/ru/usachev/wishboard/entities/Event.java
+++ src/main/java/ru/usachev/wishboard/entities/Event.java
@@ -1,4 +1,44 @@
 package ru.usachev.wishboard.entities;
 
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "event")
 public class Event {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private int id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "start_date")
+  private Date startDate;
+
+  @Column(name = "period_frequency")
+  private int periodFrequency;
+
+  @Column(name = "recurring_count")
+  private int recurringCount;
+
+  @OneToOne(cascade = CascadeType.ALL, mappedBy = "event")
+  private Task task;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "period_type_id")
+  private PeriodType periodType;
+
+
 }
