daf8fa15b7285491b99fd1f811d31c7cc00af6be
Усачев Дмитрий Дмитриевич <92usachevdd@region.cbr.ru>
2021-09-14 23:26:12 +0300

    #45

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Создать цель </h1>
+    <input v-model="wishName" placeholder="Введите название цели">
+    <button v-on:click="createWish">Создать</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      wishName: ''
+    }
+  },
+  methods: {
+    createWish() {
+      console.log(this.wishName)
+    }
   }
 }
 </script>
@@ -22,7 +29,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  vertical-align: middle;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ frontend/src/components/CreateWish.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="create_wish">
+    <input v-model="wishName" placeholder="Введите название цели"/>
+    <br/>
+    Название цели: {{ wishName }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateWIsh.vue",
+  data() {
+    return {
+      wishName: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
